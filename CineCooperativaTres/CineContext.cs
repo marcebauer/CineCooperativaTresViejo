da2edb0a18b7635d5597247732b5a0ea8271c0e9
laruwu <larakanevsky@gmail.com>
2022-10-19 22:44:45 -0300

    contexto

---
+++ CineCooperativaTres/CineContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CineCooperativaTres
+{
+	class CineContext : DbContext
+	{
+		public DbSet<Pelicula> Pelicula { get; set; }
+		
+		protected override void OnConfiguring(DbContextOptioinsBuilder optioinsBuilder)
+		{
+			optioinsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDBCF
+;Trusted_Connection=True;"));
+		}
+	}
+}
